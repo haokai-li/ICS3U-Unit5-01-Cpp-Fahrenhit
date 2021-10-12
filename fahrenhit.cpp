3d99c6478bcb43e790c9eb25ee2a571e7a3cdaf7
Ubuntu <ubuntu@ip-172-31-68-156.ec2.internal>
2021-10-12 17:24:47 +0000

    fahrenhit

---
+++ fahrenhit.cpp
@@ -0,0 +1,39 @@
+// Copyright (c) 2021 haokai All rights reserved
+//
+// Created by: Haokai
+// Created on: Oct 2021
+// This Program is about loop and if
+#include <iostream>
+#include <string>
+
+void calculateFahrenhit() {
+    // This Program is about loop and if
+    std::string celiusString;
+    float celiusNumber;
+
+    float fahrenhitNumber;
+
+    std::cout << "Please enter a temperature (°C): ";
+    std::cin >> celiusString;
+    std::cout << "" << std::endl;
+
+    // process
+    try {
+        celiusNumber = std::stof(celiusString);
+
+        fahrenhitNumber = (9.0 / 5.0) * celiusNumber + 32;
+        // output
+        std::cout << celiusNumber << "°C is equal to "
+        << fahrenhitNumber << "°F." << std::endl;
+    } catch (std::invalid_argument) {
+        // output
+        std::cout << "You didn't enter an integer." << std::endl;
+        std::cout << "" << std::endl;
+    }
+
+    std::cout << "\nDone." << std::endl;
+}
+
+main() {
+    calculateFahrenhit();
+}
